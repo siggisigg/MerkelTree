1525866335d2139339d23d5edb660aea43e952ed
siggisigg <sig.ag.sig@gmail.com>
2021-12-07 12:59:55 +0000

    Class setup

---
+++ Merkeltree.cpp
@@ -0,0 +1,32 @@
+#include <string.h>
+
+class MerkleNode{
+    public:
+
+    MarkleTree(){};
+
+    private:
+
+    int size;
+    std::string Keyhash;
+    MerkleNode *Parent;
+    MerkleNode *Child;
+
+}
+//Syncronize
+
+// Insert 
+
+// Delete 
+
+// Check size
+
+
+
+
+void Main(){
+
+    std::hash;
+
+
+}
